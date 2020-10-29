46120766013d52017ef0d778ab9730cf2556b79a
Robert-BTGS <58185211+Robert-BTGS@users.noreply.github.com>
2020-10-28 23:41:47 -0400

    Add project files.

---
+++ StringRotator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace StringRotator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting StringRotator Program --- Robert Aldridge 2020");
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine("");
+
+            string BaseString = ("MyString");
+            int CharacterLimit = BaseString.Length;
+            int UserInput = GetUserInput(CharacterLimit);
+            
+            Console.WriteLine(BaseString.Substring(BaseString.Length - UserInput) + BaseString.Remove(BaseString.Length - UserInput));
+
+            static int GetUserInput(int CharacterLimit)
+            {
+                Console.WriteLine("How many characters would you like to rotate?");
+                string ConsoleInput = Console.ReadLine();
+                int UserInputInt;
+
+                if (int.TryParse(ConsoleInput, out UserInputInt) && (UserInputInt <= CharacterLimit) && (UserInputInt > 0))
+                {
+                    return UserInputInt;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input! Input must be a number and be between 1 and " + CharacterLimit + ". Please try again.");
+                    Console.WriteLine();
+
+                    return GetUserInput(CharacterLimit);
+                }
+            }
+        }
+    }
+}
